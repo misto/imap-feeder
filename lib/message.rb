002fb2b8ed6d1be088ad4df6eca327244108c6ee
mirko <mirko@19490622-df27-0410-8391-b4ffd6aba19e>
2007-02-23 22:09:37 +0000

    formatting

--- lib/message.rb
+++ lib/message.rb
@@ -1,13 +1,16 @@
 require 'base64'
+require 'action_mailer'
 
 class Message
+
+  include ActionMailer::Quoting
 
   attr_reader :title, :body, :time, :id, :from
 
   def initialize(params)
     @title = params[:title] || ""
     @from  = params[:from]  || ""
-    @body  = params[:body]  || ""
+    @body  = strip_html(params[:body]  || "")
     @id    = params[:id]    || 0
     @time  = params[:time]  || Time.now
   end
@@ -15,20 +18,45 @@ class Message
   def format
     return <<-EOF
 Date: #{@time}
-Subject: #{format_title}
-From: #{@from}
+Subject: #{quote_if_necessary(@title, "UTF-8")}
+From: #{quote_if_necessary(@from, "UTF-8")}
+Content-Type: text/plain;
+  charset=UTF-8;
 
 #{@body}
 EOF
   end
   
   private
-  def format_title
-    #what don't we need to escape? everything in 7 bit?
-    if @title =~ /^[\w\s:-]+$/
-      @title
-    else
-      "=?utf-8?b?#{Base64.encode64(@title).gsub(/\n/, '')}?="
+  
+  def strip_html(body)
+    #replace <p>
+    body.gsub!(/^<\s*p\s*>/, '')
+    body.gsub!(/<\s*\/\s*p\s*>$/, '')
+    body.gsub!(/<\s*p\s*>|<\s*\/\s*p\s*>/, "\n")
+    
+    #replace <br>
+    body.gsub!(/<\s*br\s*\/?\s*>/, "\n")
+    
+    #replace <strong>
+    body.gsub!(/<\s*\/?\s*strong\s*>/, "*")
+    
+    #sanitize newlines
+    body.gsub!(/\n{3,}/, "\n\n")
+    
+    urls = []
+    body.gsub!(/<a.*?href=\"(.*?)\".*?>(.*?)<\s*\/\s*a\s*>/) do 
+      urls << $1
+      "#{$2}[#{urls.length}]"
     end
+    
+    unless urls.empty?
+      body << "\n"
+      urls.each_with_index do |url, i|
+        body << "\n[#{(i+1).to_s}] #{url}"
+      end
+    end
+
+    body
   end
 end
--- test/test_message.rb
+++ test/test_message.rb
@@ -35,6 +35,8 @@ class MessageTest < Test::Unit::TestCase
 Date: #{t}
 Subject: title
 From: 
+Content-Type: text/plain;
+  charset=UTF-8;
 
 body
 EOF
@@ -47,6 +49,8 @@ EOF
 Date: #{t}
 Subject: title
 From: 
+Content-Type: text/plain;
+  charset=UTF-8;
 
 body
 second
@@ -56,14 +60,104 @@ EOF
   def test_format_encoded
     t = Time.now
     m = Message.new(:title => "Alexander H. Færøy: Meeting friends for the first time…", :body => "body\nsecond", :time => t)
-    assert_equal(<<EOF, m.format)
+    assert_equal(<<-EOF, m.format)
 Date: #{t}
-Subject: =?utf-8?b?QWxleGFuZGVyIEguIEbDpnLDuHk6IE1lZXRpbmcgZnJpZW5kcyBmb3IgdGhlIGZpcnN0IHRpbWXigKY=?=
+Subject: =?UTF-8?Q?Alexander_H=2e_F=c3=a6r=c3=b8y=3a_Meeting_friends_for_the_first_time=e2=80=a6?=
 From: 
+Content-Type: text/plain;
+  charset=UTF-8;
 
 body
 second
 EOF
   end
+end
+
+
+class MessageFormatterTest < Test::Unit::TestCase
+
+  def create_message body
+    Message.new(:body => body)
+  end
+
+  #
+  # Paragraphs
+  #
+  def test_one_p
+    m = create_message "<p>X</p>"
+    assert_equal("X", m.body)
+  end
   
+  def test_one_p_with_space
+    m = create_message "<  p >X<  /  p  >"
+    assert_equal("X", m.body)
+  end
+  
+  def test_one_p_in_the_middle
+    m = create_message "X<p>Y</p>Z"
+    assert_equal("X\nY\nZ", m.body)
+  end
+  
+  def test_p_with_spaces
+    m = create_message "X<p >Y< / p >Z"
+    assert_equal("X\nY\nZ", m.body)
+  end
+  
+  def test_mutliple_p
+    m = create_message "<p>Y</p><p>Z</p>"
+    assert_equal("Y\n\nZ", m.body)
+  end
+  
+  #
+  # Linebreak
+  #
+  def test_br_html
+    m = create_message "Y<br>Z"
+    assert_equal("Y\nZ", m.body)
+  end
+  
+  def test_br_xhtml
+    m = create_message "Y<br/>Z"
+    assert_equal("Y\nZ", m.body)
+  end
+  
+  def test_br_with_space
+    m = create_message "Y<br />Z"
+    assert_equal("Y\nZ", m.body)
+  end
+  
+  #
+  # Lines
+  #
+  def test_multi_lines
+    m = create_message "Y<br /><br /><br /><br /><br />Z"
+    assert_equal("Y\n\nZ", m.body)
+  end    
+  
+  #
+  # Strong
+  #
+  def test_multi_lines
+    m = create_message "< strong >g<  / strong>"
+    assert_equal("*g*", m.body)
+  end  
+  
+  #
+  # URLs
+  #
+  def test_href
+    m = create_message "Kuck mal <a href=\"http://da.da\">hier</a>!"
+    assert_equal("Kuck mal hier[1]!\n\n[1] http://da.da", m.body)
+  end
+  
+  def test_hrefs
+    m = create_message "Kuck mal <a href=\"http://da.da\">hier</a> und <a   href=\"http://namal.da\">da</ a>!"
+    assert_equal(<<EOS.chomp, m.body)
+Kuck mal hier[1] und da[2]!
+
+[1] http://da.da
+[2] http://namal.da
+EOS
+  end
+
 end
